0365a7c559ca0c6a0d3864f96289eb2bcc335948
rtarun1 <tarun.ramak@gmail.com>
2025-08-05 16:27:00 +0530

    added intensity

--- obstacle_handling/human_tracking.py
+++ obstacle_handling/human_tracking.py
@@ -2,6 +2,7 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image, PointCloud2, PointField, CameraInfo
+from sensor_msgs_py import point_cloud2
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
 from cv_bridge import CvBridge
@@ -12,48 +13,16 @@ import ros2_numpy
 from sklearn.cluster import DBSCAN
 import cv2
 
-def create_point_cloud_xyz_intensity(header, points_xyz, intensity):
-
-    fields = [
-        PointField(name='x', offset=0, datatype=PointField.FLOAT32, count=1),
-        PointField(name='y', offset=4, datatype=PointField.FLOAT32, count=1),
-        PointField(name='z', offset=8, datatype=PointField.FLOAT32, count=1),
-        PointField(name='intensity', offset=12, datatype=PointField.FLOAT32, count=1)
-    ]
-    
-    # Create the PointCloud2 message
-    points = np.hstack((points_xyz, intensity))
-    msg = PointCloud2()
-    msg.header = header
-    msg.height = 1
-    msg.width = len(points)
-    msg.is_dense = True  # Assumes no NaN or Inf values
-    msg.is_bigendian = False
-    msg.fields = fields
-    
-    # Set the point_step (size of a single point in bytes)
-    msg.point_step = 16  # 3 fields * 4 bytes/float
-    
-    # Set the row_step (total size of a row in bytes)
-    msg.row_step = msg.point_step * msg.width
-    
-    # Convert the numpy array to a byte array and assign it to the data field
-    msg.data = np.asarray(points, dtype=np.float32).tobytes()
-    
-    return msg
-
 
 class HumanDetectorNode(Node):
     def __init__(self):
-        super().__init__("yolov8_human_detector")
+        super().__init__("yolo_human_detector")
        
         self.bridge = CvBridge()
         self.model = YOLO('yolo11n-seg.pt')
         self.model.to('cuda')
         self.conf_threshold = 0.5
         self.target_class = 0
-        self.camera_frame = 'camera1_link'
-        self.lidar_frame = 'livox_frame'
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
         self.camera_intrinsics = None
@@ -106,10 +75,6 @@ class HumanDetectorNode(Node):
             self.get_logger().warn("Waiting for camera intrinsics...")
             return
         
-        # transform = self.tf_buffer.lookup_transform(
-        #     self.camera_frame, self.lidar_frame, rclpy.time.Time()
-        # )
-        
         # 1. Hardcode your provided extrinsic values [x, y, z, qx, qy, qz, qw]
         # This is T_lidar_camera (transforms from camera to lidar)
         T_lidar_camera_arr = [
@@ -120,6 +85,7 @@ class HumanDetectorNode(Node):
         rotation_m = ros2_numpy.geometry.transformations.quaternion_matrix(T_lidar_camera_arr[3:7])
         T_lidar_camera = np.dot(translation_m, rotation_m)
         transform_matrix = np.linalg.inv(T_lidar_camera)
+        # print(transform_matrix)
 
         cv_image = self.bridge.imgmsg_to_cv2(image_msg, desired_encoding="bgr8")
         
@@ -138,6 +104,7 @@ class HumanDetectorNode(Node):
 
         combined_mask = np.zeros((image_msg.height, image_msg.width), dtype=np.uint8)
         target_shape = (image_msg.width, image_msg.height)
+
         for mask_data in results[0].masks.data:
             small_mask = mask_data.cpu().numpy().astype(np.uint8)
             resized_mask = cv2.resize(small_mask, target_shape, interpolation=cv2.INTER_NEAREST)
@@ -148,15 +115,11 @@ class HumanDetectorNode(Node):
         segmentation_msg.header = image_msg.header 
         self.seg_image_pub.publish(segmentation_msg)
 
-        pc_dict = ros2_numpy.point_cloud2.point_cloud2_to_array(pc_msg)
-        points = pc_dict['xyz']
-        intensity = pc_dict['intensity']
+        cloud_array = ros2_numpy.point_cloud2.pointcloud2_to_array(pc_msg)
 
-        # t = transform.transform.translation
-        # q = transform.transform.rotation
-        # translation_matrix = ros2_numpy.geometry.transformations.translation_matrix([t.x, t.y, t.z])
-        # rotation_matrix = ros2_numpy.geometry.transformations.quaternion_matrix([q.x, q.y, q.z, q.w])
-        # transform_matrix = np.dot(translation_matrix, rotation_matrix) # by this we get the transformation between camera and lidar
+        points = ros2_numpy.point_cloud2.get_xyz_points(cloud_array, remove_nans=False)
+        intensities = None
+        intensities = cloud_array['intensity']
 
         points_homogeneous = np.hstack((points, np.ones((points.shape[0], 1))))
         points_camera_frame = (transform_matrix @ points_homogeneous.T).T[:, :3]
@@ -164,9 +127,9 @@ class HumanDetectorNode(Node):
         in_front_mask = points_camera_frame[:, 2] > 0
         points_for_projection = points_camera_frame[in_front_mask]
         original_points_for_publishing = points[in_front_mask]
-        original_intensity_for_publishing = intensity[in_front_mask]
-
-
+        if intensities is not None:
+            intensities_in_front = intensities[in_front_mask]
+        
         if len(points_for_projection) == 0:
             return
 
@@ -178,9 +141,10 @@ class HumanDetectorNode(Node):
         
         pixel_coords_on_image = pixel_coords[on_image_mask]
         original_points_on_image = original_points_for_publishing[on_image_mask]
-        original_intensity_on_image = original_intensity_for_publishing[on_image_mask]
 
-
+        if intensities is not None:
+            intensities_on_image = intensities_in_front[on_image_mask]
+        
         if len(pixel_coords_on_image) == 0:
             return
         
@@ -188,14 +152,25 @@ class HumanDetectorNode(Node):
         human_points_mask = (mask_values == 255)
 
         final_points = original_points_on_image[human_points_mask]
-        final_intensity = original_intensity_on_image[human_points_mask]
 
+        if intensities is not None:
+            final_intensities = intensities_on_image[human_points_mask]
 
         if len(final_points) > 0:
-            header = pc_msg.header
-            filtered_pc_msg = create_point_cloud_xyz_intensity(header, final_points, final_intensity)
+            dtype = [('x', 'f4'), ('y', 'f4'), ('z', 'f4')]
+            dtype.append(('intensity', 'u2'))
+            output_array = np.zeros(len(final_points), dtype=dtype)
+            output_array['x'] = final_points[:, 0]
+            output_array['y'] = final_points[:, 1]
+            output_array['z'] = final_points[:, 2]
+            output_array['intensity'] = final_intensities
+            final_pc_msg = ros2_numpy.point_cloud2.array_to_pointcloud2(
+                output_array, 
+                stamp=pc_msg.header.stamp, 
+                frame_id=pc_msg.header.frame_id
+            )
 
-            self.seg_pc_pub.publish(filtered_pc_msg)
+            self.seg_pc_pub.publish(final_pc_msg)
 
 
 def main(args=None):
